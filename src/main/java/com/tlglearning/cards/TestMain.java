2869032ceee78a3b74a3d634075e0e84784bc83f
DennisTrujilloDev <dennis.joel.trujillo@gmail.com>
2022-07-29 16:50:54 -0400

    rank-first comparator as anon class in main

--- src/main/java/com/tlglearning/cards/TestMain.java
+++ src/main/java/com/tlglearning/cards/TestMain.java
@@ -26,20 +26,21 @@ public class TestMain {
     deck.sort();
     System.out.println(deck);
 
-    deck.sort(new RankFirstComparator());
+
+deck.sort(new Comparator<Card>(){
+
+      @Override
+      public int compare(Card card1, Card card2) {
+        int comparison = card1.getRank().compareTo(card2.getRank());
+        if(comparison == 0){
+          comparison = card1.getSuit().compareTo(card2.getSuit());
+        }
+        return comparison;
+      }
+    });
+
     System.out.println(deck);
 
   }
 
-  static class RankFirstComparator implements Comparator<Card> {
-
-    @Override
-    public int compare(Card card1, Card card2) {
-      int comparison = card1.getRank().compareTo(card2.getRank());
-      if(comparison == 0){
-        comparison = card1.getSuit().compareTo(card2.getSuit());
-      }
-      return comparison;
-    }
-  }
 }
