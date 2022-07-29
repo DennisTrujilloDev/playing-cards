9e9296b785d00ec64e4aea4d380e0c45049e9056
DennisTrujilloDev <dennis.joel.trujillo@gmail.com>
2022-07-29 16:17:54 -0400

    added comparator

--- src/main/java/com/tlglearning/cards/TestMain.java
+++ src/main/java/com/tlglearning/cards/TestMain.java
@@ -2,6 +2,7 @@ package com.tlglearning.cards;
 
 import com.tlglearning.cards.model.Card;
 import com.tlglearning.cards.model.Deck;
+import com.tlglearning.cards.strategy.RankFirstComparator;
 import java.security.SecureRandom;
 import java.util.Random;
 
@@ -10,14 +11,24 @@ public class TestMain {
   public static void main(String[] args) {
     Deck deck = new Deck();
     Random rng = new SecureRandom();
+    System.out.println(deck);
+
+    deck.shuffle(rng);
     //the reference type is the most general possible
     //that still gives you access to all the behavior you need
     System.out.println(deck);
-    for(Card card: deck){
+//    for(Card card: deck){
+//
+//    }
 
-    }
     //when you pass an object to print, if its not already
     //a string, its toString method will be printed
+    deck.sort();
+    System.out.println(deck);
+
+    deck.sort(new RankFirstComparator());
+    System.out.println(deck);
+
   }
 
 }
